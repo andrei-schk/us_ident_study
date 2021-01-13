980ead981c4d18dc3a4890de642c9c68d1d067a8
andrei-schk <77106673+andrei-schk@users.noreply.github.com>
2021-01-13 15:50:08 +0300

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 THEWAY=$(pwd)
 #git clone https://github.com/andrei-schk/us_ident_study.git
-unzip ./capstone_user_identification/3users
-unzip ./capstone_user_identification/10users
-unzip ./capstone_user_identification/150users
+unzip ./capstone_user_identification/3users.zip
+unzip ./capstone_user_identification/10users.zip
+unzip ./capstone_user_identification/150users.zip
 docker run -v $THEWAY/capstone_user_identification -p 8888:8888 andreischk/user_identification_study
