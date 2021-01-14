4a034ec4cafee5ca3a46163c7fcde7bea99b4757
andrei-schk <77106673+andrei-schk@users.noreply.github.com>
2021-01-15 02:37:01 +0300

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -2,8 +2,8 @@
 THEWAY=$(pwd)
 
 unzip ./capstone_user_identification/3users.zip -d ./capstone_user_identification
-unzip /capstone_user_identification/10users.zip -d ./capstone_user_identification
-unzip /capstone_user_identification/150users.zip -d ./capstone_user_identification
+unzip ./capstone_user_identification/10users.zip -d ./capstone_user_identification
+unzip ./capstone_user_identification/150users.zip -d ./capstone_user_identification
 
 docker image build -t user_identification_study .
 docker run -v $THEWAY/capstone_user_identification:/notebooks/capstone_user_identification -p 8888:8888 user_identification_study
